86411de23a01f54f545f578adc5b222c72daf3fc
HailTheCzar <71359784+HailTheCzar@users.noreply.github.com>
2021-12-06 01:56:20 -0500

    Updated Job and Skill classes.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,10 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
     String description;
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
     public String getDescription() {
         return description;
